4947bdc14f9b605553427b383b36b6dbc3ae951f
621948 <621948@student.inholland.nl>
2024-12-12 16:26:14 +0100

    removed unnused imports

--- app/src/main/java/com/example/exercise3/ui/screens/AnimatedSpecificScreen.kt
+++ app/src/main/java/com/example/exercise3/ui/screens/AnimatedSpecificScreen.kt
@@ -14,7 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.exercise3.R
+
 import com.example.exercise3.viewmodels.AnimatedSpecificViewModel
 import kotlinx.coroutines.flow.collectLatest
 
